d313b6c0be7a25221c086d1a21ca3fd5b5c741a7
minJonRing <1014080407tqr@gmail.com>
2019-03-18 17:11:19 +0800

    1

---
+++ src/components/app.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app">
+        <div class="head">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+        return {
+        }
+        
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+html,body,.app {
+    height: 100%;
+}
+</style>
+
